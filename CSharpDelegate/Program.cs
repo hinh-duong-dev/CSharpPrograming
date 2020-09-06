dfb616898f1656b81a8042fd5b9dab7a04ab2d11
hinh.duong <duonghinh0402@gmail.com>
2020-09-06 23:51:07 +0700

    Add project files.

---
+++ CSharpDelegate/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace CSharpDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Action delegate
+            Action<int, int> Addition = AddNumbers;
+            Addition(10, 20);
+            Console.WriteLine($"Addition = {resultAction}");
+
+            Action<int, int> AdditionAnonymous = delegate (int param1, int param2)
+            {
+                resultAction = param1 + param2;
+            };
+            AdditionAnonymous(10, 20);
+            Console.WriteLine($"AdditionAnonymous = {resultAction}");
+
+            Action<int, int> AdditionLambda = (param1, param2) => resultAction = param1 + param2; ;
+            AdditionLambda(10, 20);
+            Console.WriteLine($"AdditionLambda = {resultAction}");
+
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+
+            //Predicate delegate
+            Predicate<string> CheckIfApple = IsApple;
+            bool resultPredicate = IsApple("I Phone X");
+            if (resultPredicate)
+                Console.WriteLine("It's an IPhone");
+
+
+            Predicate<string> CheckIfAppleAnonymous = delegate (string modelName) {
+                if (modelName == "I Phone X") return true;
+                else return false;
+            };
+            bool resultPredicateAnonymous = CheckIfAppleAnonymous("I Phone X");
+            if (resultPredicateAnonymous) Console.WriteLine("It's an IPhone");
+
+
+            Predicate<string> CheckIfAppleLambda = modelName => {
+                if (modelName == "I Phone X") return true;
+                else return false;
+            };
+            bool resultPredicateLambda = CheckIfAppleLambda("I Phone X");
+            if (resultPredicateLambda) Console.WriteLine("It's an IPhone");
+
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+
+            //Func delegate
+            Func<int, int, int> Summation = SumNumber;
+            int resultFunc = Summation(10, 20);
+            Console.WriteLine($"Summation = {resultFunc}");
+
+            Func<int, int, int> SummationAnonymous = delegate (int param1, int param2)
+            {
+                return param1 + param2;
+            };
+            int resultFuncAnonymos = SummationAnonymous(10, 20);
+            Console.WriteLine($"SummationAnonymous = {resultFuncAnonymos}");
+
+            Func<int, int, int> SummationLambda = (param1, param2) => param1 + param2;
+            int resultFuncLambda = SummationLambda(10, 20);
+            Console.WriteLine($"SummationLambda = {resultFuncLambda}");
+
+
+            Console.ReadKey();
+        }
+
+        //For Action delegate
+        private static int resultAction;
+        private static void AddNumbers(int param1, int param2)
+        {
+            resultAction = param1 + param2;
+        }
+
+        //For Predicate delegate
+        private static bool IsApple(string modelName)
+        {
+            if (modelName == "I Phone X")
+                return true;
+            else
+                return false;
+        }
+
+        //For Func delegate
+        private static int SumNumber(int param1, int param2)
+        {
+            return param1 + param2;
+        }
+    }
+}
---
+++ CSharpGeneric/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharpGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstInt = 1;
+            int secondInt = 2;
+            Swap(ref firstInt, ref secondInt);
+            Console.WriteLine("firstInt: " + firstInt);
+            Console.WriteLine("secondInt: " + secondInt);
+
+            double firstDouble = 1.5;
+            double secondDouble = 2.5;
+            Swap(ref firstDouble, ref secondDouble);
+            Console.WriteLine("firstDouble: " + firstDouble);
+            Console.WriteLine("secondDouble " + secondDouble);
+
+            Console.ReadKey();
+
+        }
+
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = b;
+            b = a;
+            a = temp;
+        }
+    }
+
+}
